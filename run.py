97576bfc992e3ff891be195b69406ecc0911ca92
settegaetano <38261166+settegaetano@users.noreply.github.com>
2020-12-15 20:29:48 +0100

    Update news recommendation

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ import pandas as pd
 from scipy import spatial
 from os import walk
 import os
+from operator import itemgetter
 
 
 class Technique(Enum):
@@ -43,48 +44,64 @@ def calculate_centroid(text):
             vector = wv[word]
             vectors.append(vector)
         except Exception:
-            #print('Skipping word {}'.format(word))
+            # print('Skipping word {}'.format(word))
             continue
     if vectors:
         return np.asarray(vectors).mean(axis=0)
     return np.array([])
 
 
-def getAllNews(fileName,email,query):
+def getNewsRecommendation(fileName, email, query):
+
+    userNews = []  # Contiene una lista di news per un utente con [email, link news, cosine]
+
     with open(fileName, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
+
             if row[5] and len(row[5]) > 10:
+                news = [] # creo lista vuota che ocnterrà le info dell'utente per una determinata news
 
                 pp_news = pp.preprocess_string(row[5],
-                                           CUSTOM_FILTERS)  # Faccio il pre-processing della descrizione della notizia
+                                               CUSTOM_FILTERS)  # Faccio il pre-processing della descrizione della notizia
 
                 # Calcolo il centroide per ogni news
                 newsVector = calculate_centroid(pp_news)
 
                 # Calcolo la cosine similarity tra il centroide della news ed il centroide della preferenza dell'utente
                 cos_sim = 1 - spatial.distance.cosine(query, newsVector)
-                # Ordino in ordine decrescente la cosine similarity
-                # cosine_similarities.sort(reverse=True)
-                with io.open("ratings.csv", "a", encoding="utf-8") as myfile:
-                    myfile.write(email+ ";")
-                    myfile.write(row[1]+";")
-                    myfile.write('{} \n'.format(cos_sim))
-                    #print(pp_news) # descrizione pre-processata
+
+                # Info sulla news
+                news.append(email)  # email
+                news.append(row[1])  # link
+                news.append(cos_sim)  # cosine similarity
+
+                # Inserisco la news in una lista di news
+                userNews.append(news)
+
+    file.close()
+
+    # Ordino in ordine decrescente la cosine similarity
+    userNews.sort(key=itemgetter(2), reverse=True)
+
+    #Scrivo nel file i ratings per le news dell'utente
+    with io.open("ratings.csv", "a", encoding="utf-8") as myfile:
+
+        for news in userNews:
+            myfile.write(news[0] + ";")
+            myfile.write(news[1] + ";")
+            myfile.write('{} \n'.format(news[2]))
+            # print(pp_news) # descrizione pre-processata
 
     myfile.close()
-    file.close()
+
     return ''
 
 
+def readProfile(file, email):
 
-
-
-
-def readProfile(file,email):
-    # Lettura file JSON
     # Apertura file JSON
-    f = open('fileMyrror/'+file)
+    f = open('fileMyrror/' + file)
 
     # Ritorna JSON come oggetto
     data = json.load(f)
@@ -103,24 +120,24 @@ def readProfile(file,email):
             data = i['value'].replace('Like:', '').replace('Topic:', '').split()
             preferencePositive.append(data)
 
-
-
     if preferencePositive and preferencePositive != [] and len(preferencePositive) >= 2:
-        #print(email)
         # Trasformo la lista in un array numpy e riduco la dimensionalità ad un singolo vettore
         preferencePositiveArray = np.hstack(preferencePositive)
+
         # Calcolo il centroide delle preferenze positive dell'utente per le news
         query = calculate_centroid(preferencePositiveArray)
+
         # Lettura descrizione degli articoli presenti nel file news.csv
-        allNews = getAllNews('newsIta.csv',email,query)
+        allNews = getNewsRecommendation('newsIta.csv', email, query)
 
 
 # Utilizzo Word2Vec
 wv = api.load('word2vec-google-news-300')
 
+# Lettura file degli utenti di Myrror
 for (dirpath, dirnames, filenames) in walk("fileMyrror"):
     for file in filenames:
         if file.startswith('past_'):
             email = file.split("past_", 1)[1]
             email = os.path.splitext(email)[0]
-            readProfile(file,email)
+            readProfile(file, email)
