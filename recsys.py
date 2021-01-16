913f33be0bd3e2eab275b88f7944b9659f267396
rakes7 <giannettis97@gmail.com>
2021-01-16 10:54:22 +0100

    w2v
    w2v addestrato

--- recsys.py
+++ recsys.py
@@ -9,8 +9,11 @@ from scipy import spatial
 from os import walk
 import os
 from operator import itemgetter
-from gensim.models.fasttext import load_facebook_model
+#from gensim.models.fasttext import load_facebook_model
+from gensim.models.wrappers import FastText
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models import Word2Vec
+from gensim.models.wrappers import FastText
 
 # Pre-processing operations to apply
 CUSTOM_FILTERS = [lambda x: x.lower(), pp.strip_tags,
@@ -19,25 +22,20 @@ CUSTOM_FILTERS = [lambda x: x.lower(), pp.strip_tags,
                   # pp.split_alphanum,
                   pp.strip_multiple_whitespaces]
 
-NEWS_FILTERS = [lambda x: x.lower(), pp.strip_tags,
-                pp.strip_punctuation,
-                pp.split_alphanum, pp.strip_multiple_whitespaces,
-                pp.strip_numeric]
 
-
-def calculate_centroid(text, modeApi):
+def calculate_centroid(text, modeApi,wv):
     vectors = list()
     for word in text:
         try:
-            if modeApi == 1:  # Word2Vec
+           # if modeApi == 1:  # Word2Vec
                 vector = wv[word]
                 vectors.append(vector)
-            elif modeApi == 2:  # FastText
-                vector = ft[word]
-                vectors.append(vector)
-            elif modeApi == 3:  # Doc2Vec
-                vector = dv[word]
-                vectors.append(vector)
+            #elif modeApi == 2:  # FastText
+            #    vector = ft[word]
+            #    vectors.append(vector)
+            #elif modeApi == 3:  # Doc2Vec
+            #    vector = dv[word]
+            #    vectors.append(vector)
         except Exception:
             # print('Skipping word {}'.format(word))
             continue
@@ -46,9 +44,9 @@ def calculate_centroid(text, modeApi):
     return np.array([])
 
 
-def getNewsRecommendationW2V(fileName, email, preference, fileRatings, alreadyLiked):
+def getNewsRecommendationW2V(fileName, email, preference, fileRatings, alreadyLiked,wv):
     userNews = []  # Contiene una lista di news per un utente con [email, link news, cosine]
-    query = calculate_centroid(preference, 1)
+    query = calculate_centroid(preference, 1,wv)
     with open(fileName, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
@@ -61,7 +59,7 @@ def getNewsRecommendationW2V(fileName, email, preference, fileRatings, alreadyLi
 
                 if len(pp_news) > 2:
                     # Calcolo il centroide per ogni news
-                    newsVector = calculate_centroid(pp_news, 1)
+                    newsVector = calculate_centroid(pp_news, 1,wv)
 
                     # Calcolo la cosine similarity tra il centroide della news ed il centroide della preferenza dell'utente
                     cos_sim = 1 - spatial.distance.cosine(query, newsVector)
@@ -84,13 +82,15 @@ def getNewsRecommendationW2V(fileName, email, preference, fileRatings, alreadyLi
 
         i = 0
         for news in userNews:
-            if i > 5:
+            if i > 2:
                 break
 
             if not (news[1] in alreadyLiked):
                 myfile.write(news[0] + ";")  # email
                 myfile.write(news[1] + ";")  # link
-                myfile.write('{} \n'.format(news[2]))  # cosine similarity
+                myfile.write('{} \n'.format(news[2]))
+
+                # cosine similarity
                 # print(pp_news) # descrizione pre-processata
                 i = i + 1
 
@@ -102,7 +102,7 @@ def getNewsRecommendationW2V(fileName, email, preference, fileRatings, alreadyLi
 
 def getNewsRecommendationFastText(fileName, email, preference, fileRatings, alreadyLiked):
     userNews = []  # Contiene una lista di news per un utente con [email, link news, cosine]
-    query = calculate_centroid(preference, 2)
+    query = calculate_centroid(preference, 2,ft)
     with open(fileName, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
@@ -114,7 +114,7 @@ def getNewsRecommendationFastText(fileName, email, preference, fileRatings, alre
                                                CUSTOM_FILTERS)  # Faccio il pre-processing della descrizione della notizia
 
                 # Calcolo il centroide per ogni news
-                newsVector = calculate_centroid(pp_news, 2)
+                newsVector = calculate_centroid(pp_news, 2,ft)
 
                 # Calcolo la cosine similarity tra il centroide della news ed il centroide della preferenza dell'utente
                 cos_sim = 1 - spatial.distance.cosine(query, newsVector)
@@ -137,7 +137,7 @@ def getNewsRecommendationFastText(fileName, email, preference, fileRatings, alre
         i = 0
 
         for news in userNews:
-            if i > 10:
+            if i > 5:
                 break
 
             if not (news[1] in alreadyLiked):
@@ -158,7 +158,8 @@ def getNewsRecommendationDoc2Vec(fileName, email, preference, fileRatings, alrea
     userNews = []  # Contiene una lista di news per un utente con [email, link news, cosine]
     documents = []
     links = []
-    query = calculate_centroid(preference, 3)
+    count =  0
+    query = calculate_centroid(preference, 3,dv)
     with open(fileName, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
@@ -171,11 +172,12 @@ def getNewsRecommendationDoc2Vec(fileName, email, preference, fileRatings, alrea
 
                 if len(pp_news) > 2:
                     # Calcolo il centroide per ogni news
-                    newsVector = calculate_centroid(pp_news, 3)
-
-                    # Calcolo la cosine similarity tra il centroide della news ed il centroide della preferenza dell'utente
-                    cos_sim = 1 - spatial.distance.cosine(query, newsVector)
-
+                    newsVector = calculate_centroid(pp_news, 3,dv)
+                    try:
+                        # Calcolo la cosine similarity tra il centroide della news ed il centroide della preferenza dell'utente
+                        cos_sim = 1 - spatial.distance.cosine(query, newsVector)
+                    except:
+                        cos_sim = 0
                     # Info sulla news
                     news.append(email)  # email
                     news.append(row[1])  # link
@@ -192,7 +194,7 @@ def getNewsRecommendationDoc2Vec(fileName, email, preference, fileRatings, alrea
     with io.open(fileRatings, "a", encoding="utf-8") as myfile:
         i = 0
         for news in userNews:
-            if i > 6:
+            if i > 5:
                 break
 
             if not (news[1] in alreadyLiked):
@@ -228,23 +230,8 @@ def getNewsRecommendationLsi(fileName, email, query, fileRatings, alreadyLiked, 
     dictionary = gensim.corpora.Dictionary(processed_corpus)
     bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
 
-    tfidf = gensim.models.TfidfModel(bow_corpus, smartirs='npu')
+    tfidf = gensim.models.TfidfModel(bow_corpus)
     corpus_tfidf = tfidf[bow_corpus]
-
-    '''
-    if dim > 300:
-        num_topics = 200
-    else:
-        num_topics = 100
-
-    if dim > 1000:
-        num_topics = 200
-
-    if dim > 2100:
-        num_topixs = 200
-    
-    # 2200 limit for words
-    '''
 
     num_topics = 200
 
@@ -257,7 +244,7 @@ def getNewsRecommendationLsi(fileName, email, query, fileRatings, alreadyLiked, 
     vec_bow_tfidf = tfidf[new_vec]
     sims = index[vec_bow_tfidf]
 
-    for s in sorted(enumerate(sims), key=lambda item: -item[1])[:10]:
+    for s in sorted(enumerate(sims), key=lambda item: -item[1])[:5]:
 
         if not (newsdf[s[0] - 1][1] in alreadyLiked):
             with io.open(fileRatings, "a", encoding="utf-8") as myfile:
@@ -273,7 +260,7 @@ def getNewsRecommendationLsi(fileName, email, query, fileRatings, alreadyLiked, 
     return ''
 
 
-def profileBuilder(file, email, Technique):
+def profileBuilder(file, email, Technique, wv_en,wv_it):
     # Apertura file JSON
     f = open('fileMyrror/' + file)
 
@@ -326,36 +313,46 @@ def profileBuilder(file, email, Technique):
                 else:
                     preferenceIT.append(data)
                     preferenceEN.append(data)
-                    query += " " + data
-                    query += " " + data
+                    queryIT += " " + data
+                    queryEN += " " + data
 
-    dim = len(queryEN)
+    #dim = len(queryEN)
     #if dim < 10:
-    queryEN += query
+    #queryEN += query
     dim = len(queryEN)
+    if dim > 2000:
+        cut = dim - 2000
+        queryEN = queryEN[cut:]
 
     # INGLESE
-    if dim > 10 and len(preferenceEN) >= 1:
+    if dim > 10 and len(preferenceEN) >= 2:
         preferencePositiveEN = pp.preprocess_string(queryEN, CUSTOM_FILTERS)
 
         if Technique == 1:  # Word2Vec
-            getNewsRecommendationW2V('newsEN.csv', email, preferencePositiveEN, "rec_news_w2v_en.csv", alreadyLiked)
+            #print(preferencePositiveEN)
+            getNewsRecommendationW2V('newsEn.csv', email, preferencePositiveEN, "rec_news_w2v_en.csv", alreadyLiked,wv_en)
         elif Technique == 2:  # FastText
-            getNewsRecommendationFastText('newsEN.csv', email, preferencePositiveEN, "rec_news_ft_en.csv", alreadyLiked)
+            getNewsRecommendationFastText('newsEn.csv', email, preferencePositiveEN, "rec_news_ft_en.csv", alreadyLiked)
         elif Technique == 3:  # Doc2Vec
-            getNewsRecommendationDoc2Vec('newsEN.csv', email, preferencePositiveEN, "rec_news_d2v_en.csv", alreadyLiked)
+            getNewsRecommendationDoc2Vec('newsEn.csv', email, preferencePositiveEN, "rec_news_d2v_en.csv", alreadyLiked)
         elif Technique == 4:  # Lsi
-            getNewsRecommendationLsi('newsEN.csv', email, preferencePositiveEN, "rec_news_lsi_en.csv", alreadyLiked, dim)
+            getNewsRecommendationLsi('newsEn.csv', email, preferencePositiveEN, "rec_news_lsi_en.csv", alreadyLiked, dim)
 
     # ITALIANO
     dim = len(queryIT)
     #if dim < 10:
-    queryIT += query
+    if dim > 2000:
+        cut = dim - 2000
+        queryIT = queryIT[cut:]
+
+    #queryIT += query
     if len(queryIT) > 10 and len(preferenceIT) >= 2:
         preferencePositiveIT = pp.preprocess_string(queryIT, CUSTOM_FILTERS)
 
         if Technique == 1:  # Word2Vec
-            getNewsRecommendationW2V('newsIta.csv', email, preferencePositiveIT, "rec_news_w2v_it.csv", alreadyLiked)
+            print(preferencePositiveIT)
+            getNewsRecommendationW2V('newsIta.csv', email, preferencePositiveIT, "rec_news_w2v_it.csv", alreadyLiked,
+                                     wv_it)
         elif Technique == 2:  # FastText
             getNewsRecommendationFastText('newsIta.csv', email, preferencePositiveIT, "rec_news_ft_it.csv", alreadyLiked)
         elif Technique == 3:  # Doc2Vec
@@ -365,7 +362,12 @@ def profileBuilder(file, email, Technique):
 
 
 def mainRun(Technique):
+    wv_en = []
+    wv_it = []
+
     if Technique == 1:  # Word2Vec
+        wv_en = trainW2V("newsEn.csv")
+        wv_it = trainW2V("newsIta.csv")
         print("\nTecnica di raccomandazione news: Word2Vec")
         try:
             f = open('rec_news_w2v_it.csv', 'r+')
@@ -377,6 +379,7 @@ def mainRun(Technique):
             print("Creazione file rec_news_w2v_en.csv...")
 
     elif Technique == 2:  # FastText
+
         print("\nTecnica di raccomandazione news: FastText")
         try:
             f = open('rec_news_ft_it.csv', 'r+')
@@ -389,6 +392,10 @@ def mainRun(Technique):
 
     elif Technique == 3:  # Doc2Vec
         print("\nTecnica di raccomandazione news: Doc2Vec")
+        #wv_it = trainD2V("newsEn.csv")
+        #wv_en = trainD2V("newsIta.csv")
+        wv_it = dv
+        wv_en = dv
         try:
             f = open('rec_news_d2v_it.csv', 'r+')
             f.truncate(0)
@@ -409,22 +416,74 @@ def mainRun(Technique):
             print("Creazione file rec_news_lsi_it.csv...")
             print("Creazione file rec_news_lsi_en.csv...")
 
+
+
+
+
+
+
     # Lettura file degli utenti di Myrror
     for (dirpath, dirnames, filenames) in walk("fileMyrror"):
         for file in filenames:
             if file.startswith('past_'):
                 email = file.split("past_", 1)[1]
                 email = os.path.splitext(email)[0]
-                profileBuilder(file, email, Technique)
+
+                profileBuilder(file, email, Technique,wv_en,wv_it)
+
+
+def trainW2V(fileName):
+    with open(fileName, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        corpus = []
+        for row in reader:
+            #print(row[5])
+            if row[5] and len(row[5]) > 10:
+                pp_news = pp.preprocess_string(row[5],CUSTOM_FILTERS)  #
+
+                if(len(pp_news) > 2):
+                    corpus.append(pp_news)
+
+    EMBEDDING_FILE = 'GoogleNews-vectors-negative300.bin.gz'
+    google_model = Word2Vec(size=300, window=5, min_count=2, workers=-1)
+    google_model.build_vocab(corpus)
+    google_model.intersect_word2vec_format(EMBEDDING_FILE, lockf=1.0, binary=True)
+    google_model.train(corpus, total_examples=google_model.corpus_count, epochs=5)
+    return google_model.wv.wv
+
+
+def trainD2V(fileName):
+    with open(fileName, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        corpus = []
+        for row in reader:
+            #print(row[5])
+            if row[5] and len(row[5]) > 10:
+                pp_news = pp.preprocess_string(row[5],CUSTOM_FILTERS)  #
+
+                if(len(pp_news) > 2):
+                    corpus.append(pp_news)
+
+    tagged_documents = []
+    for i, doc in enumerate(corpus):
+        tagged = TaggedDocument(doc, [i])
+        tagged_documents.append(tagged)
+
+    dv = Doc2Vec(tagged_documents, vector_size=100, window=3, min_count=10, workers=4, epochs=100)
+    dv.train(tagged_documents, total_examples=dv.corpus_count, epochs=dv.epochs)
+
+    return dv
+
 
 
 # Utilizzo Word2Vec
-wv = api.load('word2vec-google-news-300')
+#wv = api.load('word2vec-google-news-300')
+#wv = api.load('GoogleNews-vectors-negative300.bin.gz')
 
 # Utilizzo FastText
-ft = load_facebook_model('fasttext/wiki.simple.bin')
+ft = FastText.load_fasttext_format('fasttext/wiki.simple.bin')
 
 # Utilizzo Doc2Vec
 dv = Doc2Vec.load('doc2vec/doc2vec.bin')
 
-# mainRun(1)
+mainRun(3)
